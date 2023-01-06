a29677d044f06fd076c931f8d982418eb56a19f5
Lautaro <lu4ult@gmail.com>
2023-01-06 02:14:35 -0300

    update src in logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
     //console.log(props);
     return (
         <header>
-            <img src="/img/logo.png" alt="LU4ULT logo"></img>
+            <img src="./img/logo.png" alt="LU4ULT logo"></img>
             <NavBar />
             <UserLogin greeting="Hola Usuario!" />
         </header>
